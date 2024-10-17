285b910e60f75dcde2cba24b2a1d7f3e1b0b9045
Durvank <127382184+Durvank707@users.noreply.github.com>
2024-10-18 00:41:52 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ujjwal Aglawe
-          Aditya Mali
-        </p>
-        <p>
-          Ujjwal Aglawe 2222
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <p className='text-red-600'>
+                Ujjwal Aglawe
+                Aditya Mali
+            </p>
+            <p>
+                Ujjwal Aglawe 2222
+            </p>
+
+        </div>
+    );
 }
 
 export default App;
